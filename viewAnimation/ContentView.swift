10a9c5af8a00999552aa2a43f5ac5e04e68b0da5
Berke Kesgin <kesginberke9@gmail.com>
2024-09-12 09:57:55 +0300

    Initial Commit

---
+++ viewAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  viewAnimation
+//
+//  Created by Berke Kesgin on 12.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ viewAnimation/viewAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  viewAnimationApp.swift
+//  viewAnimation
+//
+//  Created by Berke Kesgin on 12.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct viewAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
